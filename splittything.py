96375b5c28071fc3a6518541440ccf3564e29fc9
Ian Campbell <icampbe@purdue.edu>
2015-03-07 01:05:37 -0500

    temp

--- splittything.py
+++ splittything.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 
+import pygame
+from pygame.locals import *
+import os
+
+def load_image(name):
+    try:
+        image = pygame.image.load(name)
+    except pygame.error, message:
+        print 'Cannot load image:', name
+        raise SystemExit, message
+    image = image.convert()
+    return image, image.get_rect()
+
 class Person(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self) #call Sprite initializer
-		self.image, self.rect = load_image('fist.bmp', -1)
+		self.image, self.rect = load_image('person.png')
 
 
 pygame.init()
@@ -21,6 +34,11 @@ person1 = Person()
 
 allsprites = pygame.sprite.RenderPlain((person1))
 
+clock = pygame.time.Clock()
+
 while True:
 	clock.tick(60)
-
+    #Handle Input Events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
